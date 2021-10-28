a6f9b45899ef1b074cb45809b44a22ee51103aac
Kate Coppess <kcoppess@stanford.edu>
2021-10-28 16:35:52 -0700

    fixed bug in moment_general; added force_general (NOT DEBUGGED)

--- point_source.py
+++ point_source.py
@@ -104,7 +104,8 @@ def moment_general(SOURCE_TYPE, pressure, depths, time, stationPos, stations, so
     SAVES         : bool                       : if True, will save final GF in savefile directory
     mt_savefile   : string                     : path to directory where final MT GF are saved
     ---RETURNS---
-    seismo_x, seismo_y, seismo_z : (# stations, # time points) : chosen deriv applied to synthetic seismograms
+    $$_x, $$_y, $$_z        : (# stations, # time points) : chosen deriv applied to syn seis if CARTESIAN
+    OR $$_r, $$_z, $$_tr                                        OR CYLINDRICAL
     """
     dt = time[2] - time[1]
 
@@ -114,16 +115,6 @@ def moment_general(SOURCE_TYPE, pressure, depths, time, stationPos, stations, so
     # converting position vectors into cylindrical coordinates (to use in SW calc)
     # +z downwards (just for SW calculations)
     stationPos_cyl = cylindrical(stationPos)
-
-    ## storing coordinates for separation vectors between point-source and seismometers
-    ## separation: (# receivers, # sources)
-    ## gamma: (# receivers, # sources, 3)
-    #separation, gamma = hp.separation_distances_vectors(stationPos, [sourcePos])
-    #gc.collect()
-
-    ## phase shift so as to eliminate some edge effects in from fourier transformation
-    ## number of time steps
-    #shift = 0
 
     # setting up low-pass filter to eliminate high frequency numerical effects
     nyq_freq = 0.5 / dt  # in Hz
@@ -161,33 +152,162 @@ def moment_general(SOURCE_TYPE, pressure, depths, time, stationPos, stations, so
     vel_tr = np.zeros((1, NN, TT), dtype='complex')
 
     for stat, ii in zip(stations, np.arange(NN)):
-        gf_time, gfs = gf.load_gfs(mt_gf_file+stat+'/', 0, time, INTERPOLATE_TIME=INTERPOLATE, SAVE=SAVES, save_file=mt_savefile, PLOT=False)
+        gf_time, gfs = gf.load_gfs(mt_gf_file+stat+'/', 0, time, INTERPOLATE_TIME=INTERPOLATE, SAVE=SAVES, 
+                                    save_file=mt_savefile, PLOT=False)
         gfs_hat = []
         for gg in gfs:
             gf_hat = np.fft.fft(gg, axis=0) * dt
             gfs_hat.append(gf_hat)
-        vel_z[0, ii] += np.fft.ifft(general_MT_hat[0,0] * gfs_hat[0][:,0], axis=-1) / dt
-        vel_z[0, ii] += np.fft.ifft(general_MT_hat[0,1] * gfs_hat[1][:,0], axis=-1) / dt
-        vel_z[0, ii] += np.fft.ifft(general_MT_hat[0,2] * gfs_hat[2][:,0], axis=-1) / dt
-        vel_z[0, ii] += np.fft.ifft(general_MT_hat[1,1] * gfs_hat[3][:,0], axis=-1) / dt
-        vel_z[0, ii] += np.fft.ifft(general_MT_hat[1,2] * gfs_hat[4][:,0], axis=-1) / dt
-        vel_z[0, ii] += np.fft.ifft(general_MT_hat[2,2] * gfs_hat[5][:,0], axis=-1) / dt
+        vel_z[0, ii] += np.fft.ifft(mom_rate_hat[0] * moment_tensor[0,0] * gfs_hat[0][:,0], axis=-1) / dt
+        vel_z[0, ii] += np.fft.ifft(mom_rate_hat[0] * moment_tensor[0,1] * gfs_hat[1][:,0], axis=-1) / dt
+        vel_z[0, ii] += np.fft.ifft(mom_rate_hat[0] * moment_tensor[0,2] * gfs_hat[2][:,0], axis=-1) / dt
+        vel_z[0, ii] += np.fft.ifft(mom_rate_hat[0] * moment_tensor[1,1] * gfs_hat[3][:,0], axis=-1) / dt
+        vel_z[0, ii] += np.fft.ifft(mom_rate_hat[0] * moment_tensor[1,2] * gfs_hat[4][:,0], axis=-1) / dt
+        vel_z[0, ii] += np.fft.ifft(mom_rate_hat[0] * moment_tensor[2,2] * gfs_hat[5][:,0], axis=-1) / dt
         gc.collect()
 
-        vel_r[0, ii] += np.fft.ifft(general_MT_hat[0,0] * gfs_hat[0][:,1], axis=-1) / dt
-        vel_r[0, ii] += np.fft.ifft(general_MT_hat[0,1] * gfs_hat[1][:,1], axis=-1) / dt
-        vel_r[0, ii] += np.fft.ifft(general_MT_hat[0,2] * gfs_hat[2][:,1], axis=-1) / dt
-        vel_r[0, ii] += np.fft.ifft(general_MT_hat[1,1] * gfs_hat[3][:,1], axis=-1) / dt
-        vel_r[0, ii] += np.fft.ifft(general_MT_hat[1,2] * gfs_hat[4][:,1], axis=-1) / dt
-        vel_r[0, ii] += np.fft.ifft(general_MT_hat[2,2] * gfs_hat[5][:,1], axis=-1) / dt
+        vel_r[0, ii] += np.fft.ifft(mom_rate_hat[0] * moment_tensor[0,0] * gfs_hat[0][:,1], axis=-1) / dt
+        vel_r[0, ii] += np.fft.ifft(mom_rate_hat[0] * moment_tensor[0,1] * gfs_hat[1][:,1], axis=-1) / dt
+        vel_r[0, ii] += np.fft.ifft(mom_rate_hat[0] * moment_tensor[0,2] * gfs_hat[2][:,1], axis=-1) / dt
+        vel_r[0, ii] += np.fft.ifft(mom_rate_hat[0] * moment_tensor[1,1] * gfs_hat[3][:,1], axis=-1) / dt
+        vel_r[0, ii] += np.fft.ifft(mom_rate_hat[0] * moment_tensor[1,2] * gfs_hat[4][:,1], axis=-1) / dt
+        vel_r[0, ii] += np.fft.ifft(mom_rate_hat[0] * moment_tensor[2,2] * gfs_hat[5][:,1], axis=-1) / dt
         gc.collect()
 
-        vel_tr[0, ii] += np.fft.ifft(general_MT_hat[0,0] * gfs_hat[0][:,2], axis=-1) / dt
-        vel_tr[0, ii] += np.fft.ifft(general_MT_hat[0,1] * gfs_hat[1][:,2], axis=-1) / dt
-        vel_tr[0, ii] += np.fft.ifft(general_MT_hat[0,2] * gfs_hat[2][:,2], axis=-1) / dt
-        vel_tr[0, ii] += np.fft.ifft(general_MT_hat[1,1] * gfs_hat[3][:,2], axis=-1) / dt
-        vel_tr[0, ii] += np.fft.ifft(general_MT_hat[1,2] * gfs_hat[4][:,2], axis=-1) / dt
-        vel_tr[0, ii] += np.fft.ifft(general_MT_hat[2,2] * gfs_hat[5][:,2], axis=-1) / dt
+        vel_tr[0, ii] += np.fft.ifft(mom_rate_hat[0] * moment_tensor[0,0] * gfs_hat[0][:,2], axis=-1) / dt
+        vel_tr[0, ii] += np.fft.ifft(mom_rate_hat[0] * moment_tensor[0,1] * gfs_hat[1][:,2], axis=-1) / dt
+        vel_tr[0, ii] += np.fft.ifft(mom_rate_hat[0] * moment_tensor[0,2] * gfs_hat[2][:,2], axis=-1) / dt
+        vel_tr[0, ii] += np.fft.ifft(mom_rate_hat[0] * moment_tensor[1,1] * gfs_hat[3][:,2], axis=-1) / dt
+        vel_tr[0, ii] += np.fft.ifft(mom_rate_hat[0] * moment_tensor[1,2] * gfs_hat[4][:,2], axis=-1) / dt
+        vel_tr[0, ii] += np.fft.ifft(mom_rate_hat[0] * moment_tensor[2,2] * gfs_hat[5][:,2], axis=-1) / dt
+
+    vel_z = np.real(vel_z)
+    vel_r = np.real(vel_r)
+    vel_tr = np.real(vel_tr)
+
+    if coord == 'CARTESIAN':
+        vel_x, vel_y = cartesian(vel_r, stationPos_cyl)
+        if deriv == 'ACC':
+            acc_x = np.gradient(vel_x[0], dt, axis=1)
+            acc_y = np.gradient(vel_y[0], dt, axis=1)
+            acc_z = np.gradient(vel_z[0], dt, axis=1)
+            return acc_x, acc_y, acc_z
+        elif deriv == 'DIS':
+            dis_x = sint.cumtrapz(vel_x[0], x=time, initial=0)
+            dis_y = sint.cumtrapz(vel_y[0], x=time, initial=0)
+            dis_z = sint.cumtrapz(vel_z[0], x=time, initial=0)
+            return dis_x, dis_y, dis_z
+        else:
+            return vel_x, vel_y, vel_z
+    else:
+        if deriv == 'ACC':
+            acc_r = np.gradient(vel_r[0], dt, axis=1)
+            acc_z = np.gradient(vel_z[0], dt, axis=1)
+            acc_tr = np.gradient(vel_tr[0], dt, axis=1)
+            return acc_r, acc_z, acc_tr
+        elif deriv == 'DIS':
+            dis_r = sint.cumtrapz(vel_r[0], x=time, initial=0)
+            dis_z = sint.cumtrapz(vel_z[0], x=time, initial=0)
+            dis_tr = sint.cumtrapz(vel_tr[0], x=time, initial=0)
+            return dis_r, dis_z, dis_tr
+        else:
+            return vel_r, vel_z, vel_tr
+
+
+def force_general(SOURCE_TYPE, pressure, depths, time, stationPos, stations, sourceParams, mediumParams, 
+                    mt_gf_file, deriv='DIS', coord = 'CYLINDRICAL', SOURCE_FILTER=False, INTERPOLATE=False, 
+                    SAVES=False, mt_savefile='greens_functions/'):
+    """
+    calculates the point source synthetic seismograms from single force contributions at given station positions
+    using loaded Green's functions
+
+    NB: all position vectors must be given in (x, y, z) and in units of m
+        +x : east
+        +y : north
+        +z : upwards
+
+        () indicate numpy arrays
+        [] indicate lists
+
+    ---INPUTS---
+    SOURCE_TYPE   : string                     : either 'CONDUIT' or 'CHAMBER'
+    pressure      : (# time points)            : CHAMBER -> chamber pressure history
+               OR : (# sources, # time points) : CONDUIT -> pressure history along conduit
+    depths        : (# sources)                : depths of grid points along conduit
+    time          : (# time points)            : time array (assumes equal time-stepping)
+    stationPos    : (# stations, 3)            : positions of accelerometer/seismometer stations centered
+                                                    around conduit axis
+    stations      : [# stations]               : station labels which will be used to load relevant GFs
+    sourceParams  : [1, (3)]                   : [conduit area (m^2) OR chamber vol (m^3),
+                                                    source position vector]
+    mediumParams  : [3]                        : [shear modulus (Pa), lame (Pa), rock density (kg/m^3)]
+    mt_gf_file    : string                     : path to directory where MT GF are stored
+    deriv         : string                     : seismogram time derivative to return
+                                                 (options: 'ACC' acceleration;
+                                                           'VEL' velocity;
+                                                           'DIS' displacement)
+    coord         : string                     : coordinate system for the synthetic seimograms
+                                                 (options: 'CARTESIAN' and 'CYLINDRICAL')
+    SOURCE_FILTER : bool                       : if True, filters source function before synth-seis calc
+    INTERPOLATE   : bool                       : if True, will interpolate loaded GF
+    SAVES         : bool                       : if True, will save final GF in savefile directory
+    mt_savefile   : string                     : path to directory where final MT GF are saved
+    ---RETURNS---
+    $$_x, $$_y, $$_z        : (# stations, # time points) : chosen deriv applied to syn seis if CARTESIAN
+    OR $$_r, $$_z, $$_tr                                        OR CYLINDRICAL
+    """
+    dt = time[2] - time[1]
+
+    sourceDim, sourcePos = sourceParams
+    mu, lame, rho_rock = mediumParams
+
+    # converting position vectors into cylindrical coordinates (to use in SW calc)
+    # +z downwards (just for SW calculations)
+    stationPos_cyl = cylindrical(stationPos)
+
+    # setting up low-pass filter to eliminate high frequency numerical effects
+    nyq_freq = 0.5 / dt  # in Hz
+    cutoff_freq = 0.03  # in Hz
+    normal_cutoff = cutoff_freq / nyq_freq
+    b, a = sg.butter(3, normal_cutoff, btype='low', analog=False)
+
+    if SOURCE_TYPE == 'CONDUIT':
+        dforce_dz = ss.moment_density(force, sourceDim, cushion=shift)
+        if SOURCE_FILTER:
+            filt = sg.lfilter(b, a, dforce_dz)
+        else:
+            filt = dforce_dz
+        force = hp.integration_trapezoid(height, np.array([filt]))
+    elif SOURCE_TYPE == 'CHAMBER':
+        force_unfil = ss.moment_density(np.array([force]), sourceDim, cushion=shift)[0]
+        if SOURCE_FILTER:
+            force = np.array([sg.lfilter(b, a, force_unfil)])
+        else:
+            force = np.array([force_unfil])
+    gc.collect()
+    
+    force_rate = np.gradient(moment, dt, axis=-1)
+    gc.collect()
+
+    TT = np.ma.size(force, axis=1)  # number of time points
+    NN = np.ma.size(stationPos, axis=0)  # number of receivers
+
+    for_rate_hat = np.fft.fft(force_rate, axis=1) * dt
+
+    vel_z = np.zeros((1, NN, TT), dtype='complex')
+    vel_r = np.zeros((1, NN, TT), dtype='complex')
+    vel_tr = np.zeros((1, NN, TT), dtype='complex')
+
+    for stat, ii in zip(stations, np.arange(NN)):
+        gf_time, gfs = gf.load_gfs(mt_gf_file+stat+'/', 1, time, INTERPOLATE_TIME=INTERPOLATE, SAVE=SAVES, save_file=sf_savefile, PLOT=False)
+        gfs_hat = []
+        for gg in gfs:
+            gf_hat = np.fft.fft(gg, axis=0) * dt
+            gfs_hat.append(gf_hat)
+        vel_z[0, ii] += np.fft.ifft(for_rate_hat[0] * gfs_hat[1][:,0], axis=-1) / dt
+        vel_r[0, ii] += np.fft.ifft(for_rate_hat[0] * gfs_hat[1][:,1], axis=-1) / dt
+        vel_tr[0, ii] += np.fft.ifft(for_rate_hat[0] * gfs_hat[1][:,2], axis=-1) / dt
+        gc.collect()
 
     vel_z = np.real(vel_z)
     vel_r = np.real(vel_r)
