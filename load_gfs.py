8930ebd2400d378eb9bbbd724551a54d6351f358
Kate Coppess <kcoppess@stanford.edu>
2021-10-28 14:39:09 -0700

    added in loading of extended GF and added in conditionals for saving and interpolating

--- load_gfs.py
+++ load_gfs.py
@@ -35,13 +35,6 @@ def load_gfs_PS(directory, srctype, time, INTERPOLATE=False, SAVE=False, save_fi
         components = ['horizontal_force.txt', 'vertical_force.txt']
     colors = ['#F0E442', '#E69F00', '#56B4E9', '#009E73', '#000000', '#E50000']
     
-    tt = len(time)
-    dt = time[2] - time[1]
-    desired_omega = np.fft.fftfreq(tt, dt) * (2 * np.pi)
-    ind = np.argwhere(desired_omega < 0)[0,0]
-    sorted_desired_omega = np.concatenate((desired_omega[ind:], desired_omega[:ind]))
-
-    
     gfs = []
     gfs_hat = []
     gf_time = np.loadtxt(directory+components[0], usecols = 0)
@@ -53,10 +46,9 @@ def load_gfs_PS(directory, srctype, time, INTERPOLATE=False, SAVE=False, save_fi
     gc.collect()
 
     gf_omega = np.fft.fftfreq(len(gf_time), gf_dt) * (2 * np.pi)
-
-
     gf_ind = np.argwhere(gf_omega < 0)[0,0]
     sorted_gf_omega = np.concatenate((gf_omega[gf_ind:], gf_omega[:gf_ind]))
+    gc.collect()
     
     if PLOT:
         for func, lab, col in zip(gfs_hat, components, colors):
@@ -64,22 +56,36 @@ def load_gfs_PS(directory, srctype, time, INTERPOLATE=False, SAVE=False, save_fi
             #plt.plot(gf_time, func[:,1], color=col, label=lab)
         #plt.show()
     new_gfs = []
-    for func, lab, col in zip(gfs_hat, components, colors):
-        sorted_func = np.concatenate((func[gf_ind:], func[:gf_ind]))
-        smooth = si.interp1d(sorted_gf_omega, sorted_func, axis=0, kind='cubic')
-        sorted_gf_hat_sm = smooth(sorted_desired_omega)
-        if PLOT:
-            #plt.plot(sorted_gf_omega[:-1], np.abs(smooth(sorted_gf_omega[:-1])[:,1]), color=col)
-            plt.plot(sorted_desired_omega, np.abs(sorted_gf_hat_sm[:,1]), '.', color=col)
-        gf_hat_sm = np.concatenate((sorted_gf_hat_sm[-ind:], sorted_gf_hat_sm[:-ind]))
-        new_gfs.append(np.fft.ifft(gf_hat_sm, axis=0) / dt)
+    if INTERPOLATE:
+        tt = len(time)
+        dt = time[2] - time[1]
+        desired_omega = np.fft.fftfreq(tt, dt) * (2 * np.pi)
+        ind = np.argwhere(desired_omega < 0)[0,0]
+        sorted_desired_omega = np.concatenate((desired_omega[ind:], desired_omega[:ind]))
+        for func, lab, col in zip(gfs_hat, components, colors):
+            sorted_func = np.concatenate((func[gf_ind:], func[:gf_ind]))
+            smooth = si.interp1d(sorted_gf_omega, sorted_func, axis=0, kind='cubic')
+            sorted_gf_hat_sm = smooth(sorted_desired_omega)
+            if PLOT:
+                #plt.plot(sorted_gf_omega[:-1], np.abs(smooth(sorted_gf_omega[:-1])[:,1]), color=col)
+                plt.plot(sorted_desired_omega, np.abs(sorted_gf_hat_sm[:,1]), '.', color=col)
+            gf_hat_sm = np.concatenate((sorted_gf_hat_sm[-ind:], sorted_gf_hat_sm[:-ind]))
+            new_gfs.append(np.fft.ifft(gf_hat_sm, axis=0) / dt)
+    else:
+        new_gfs = gfs
+    if SAVE:
+        for func, lab in zip(new_gfs, components):
+            combined = np.concatenate((np.array([time]).transpose(), func), axis=1)
+            np.savetxt(save_file+'interpolated_'+lab, combined,
+                    header="time, vertical, radial, transverse")
     if PLOT:
         plt.legend()
         plt.show()
 
     return time, new_gfs
 
-def load_gfs_ES(directory, srctype, time, INTERPOLATE=False, SAVE=False, save_file='gf', PLOT=False):
+def load_gfs_ES(directory, srctype, time, depths, INTERPOLATE_TIME=False, INTERPOLATE_SPACE=False, 
+                SAVE=False, save_file='gf', PLOT=False):
     '''
     loads in extended source Green's functions and can interpolate in time/space to get compatible
     array dimensions with desired time/depth array
@@ -92,17 +98,19 @@ def load_gfs_ES(directory, srctype, time, INTERPOLATE=False, SAVE=False, save_fi
             time    vertical    radial   transverse
 
     --INPUTS--
-    directory   : string            : path to folder holding Green's function files
-    srctype     : (1)               : source type (0: moment tensor, 1: single force)
-    time        : (# time points)   : desired time array
-    INTERPOLATE : bool              : if True, interpolate to get values at desired time
-    SAVE        : bool              : if True, saves Green's functions to save_file
-    save_file   : string            : path to where Green's functions will be saved
+    directory         : string            : path to folder holding Green's function files
+    srctype           : (1)               : source type (0: moment tensor, 1: single force)
+    time              : (# time points)   : desired time array
+    depths            : (# grid points)   : desired depth array
+    INTERPOLATE_TIME  : bool              : if True, interpolate to get values at desired time
+    INTERPOLATE_SPACE : bool              : if True, interpolate to get values at desired depths
+    SAVE              : bool              : if True, saves Green's functions to save_file
+    save_file         : string            : path to where Green's functions will be saved
     --RETURNS--
-    gf_time     : (# time points)        : desired time array
-    gfs         : [ (# time points, 3) ] : list of final Green's functions (ver, rad, tra)
-                                            if single force, 2 arrays
-                                            if moment tensor, 6 arrays
+    gf_time     : (# time points)                       : desired time array
+    gfs         : [ (# grid points, # time points, 3) ] : list of final Green's functions (ver, rad, tra)
+                                                             if single force, 2 arrays
+                                                             if moment tensor, 6 arrays
     '''
     if srctype == 0:
         components = ['Mxx.mat', '2Mxy.mat', '2Mxz.mat', 'Myy.mat', '2Myz.mat', 'Mzz.mat']
@@ -110,19 +118,13 @@ def load_gfs_ES(directory, srctype, time, INTERPOLATE=False, SAVE=False, save_fi
         components = ['horizontal_force.mat', 'vertical_force.mat']
     colors = ['#F0E442', '#E69F00', '#56B4E9', '#009E73', '#000000', '#E50000']
     
-    tt = len(time)
-    dt = time[2] - time[1]
-    desired_omega = np.fft.fftfreq(tt, dt) * (2 * np.pi)
-    ind = np.argwhere(desired_omega < 0)[0,0]
-    sorted_desired_omega = np.concatenate((desired_omega[ind:], desired_omega[:ind]))
-
-    
     gfs = []
     gfs_hat = []
-    gf_time = np.loadtxt(directory+components[0], usecols = 0)
+    gf_time = sio.loadmat(directory+'time.mat')['out']
+    gf_depths = sio.loadmat(directory+'depths.mat')['out']
     gf_dt = gf_time[1] - gf_time[0]
     for com in components:
-        gf = np.loadtxt(directory+com, usecols = (1,2,3))
+        gf = sio.loadmat(directory+com)['out']
         gfs.append(gf)
         gfs_hat.append(np.fft.fft(gf, axis=0) * gf_dt)
     gc.collect()
@@ -138,16 +140,48 @@ def load_gfs_ES(directory, srctype, time, INTERPOLATE=False, SAVE=False, save_fi
             plt.plot(gf_omega, np.abs(func[:,1]), color=col, label=lab)
             #plt.plot(gf_time, func[:,1], color=col, label=lab)
         #plt.show()
+    new_gfs1_hat = []
+    if INTERPOLATE_TIME:
+        tt = len(time)
+        dt = time[2] - time[1]
+        desired_omega = np.fft.fftfreq(tt, dt) * (2 * np.pi)
+        ind = np.argwhere(desired_omega < 0)[0,0]
+        sorted_desired_omega = np.concatenate((desired_omega[ind:], desired_omega[:ind]))
+        for func, lab, col in zip(gfs_hat, components, colors):
+            sorted_func = np.concatenate((func[gf_ind:], func[:gf_ind]))
+            smooth = si.interp1d(sorted_gf_omega, sorted_func, axis=1, kind='cubic')
+            sorted_gf_hat_sm = smooth(sorted_desired_omega)
+            if PLOT:
+                #plt.plot(sorted_gf_omega[:-1], np.abs(smooth(sorted_gf_omega[:-1])[:,1]), color=col)
+                plt.plot(sorted_desired_omega, np.abs(sorted_gf_hat_sm[:,1]), '.', color=col)
+            gf_hat_sm = np.concatenate((sorted_gf_hat_sm[-ind:], sorted_gf_hat_sm[:-ind]))
+            new_gfs1_hat.append(gf_hat_sm)
+    else:
+        new_gfs1_hat = gfs_hat
+    
+    new_gfs_hat = []
+    if INTERPOLATE_SPACE:
+        hh = len(depths)
+        dh = depths[2] - depths[1]
+        desired_omega = np.fft.fftfreq(hh, dh) * (2 * np.pi)
+        ind = np.argwhere(desired_omega < 0)[0,0]
+        sorted_desired_omega = np.concatenate((desired_omega[ind:], desired_omega[:ind]))
+        for func, lab, col in zip(new_gfs1_hat, components, colors):
+            sorted_func = np.concatenate((func[gf_ind:], func[:gf_ind]))
+            smooth = si.interp1d(sorted_gf_omega, sorted_func, axis=0, kind='cubic')
+            sorted_gf_hat_sm = smooth(sorted_desired_omega)
+            if PLOT:
+                #plt.plot(sorted_gf_omega[:-1], np.abs(smooth(sorted_gf_omega[:-1])[:,1]), color=col)
+                plt.plot(sorted_desired_omega, np.abs(sorted_gf_hat_sm[:,1]), '.', color=col)
+            gf_hat_sm = np.concatenate((sorted_gf_hat_sm[-ind:], sorted_gf_hat_sm[:-ind]))
+            new_gfs.append(gf_hat_sm)
+    else:
+        new_gfs_hat = new_gfs1_hat
+    
     new_gfs = []
-    for func, lab, col in zip(gfs_hat, components, colors):
-        sorted_func = np.concatenate((func[gf_ind:], func[:gf_ind]))
-        smooth = si.interp1d(sorted_gf_omega, sorted_func, axis=0, kind='cubic')
-        sorted_gf_hat_sm = smooth(sorted_desired_omega)
-        if PLOT:
-            #plt.plot(sorted_gf_omega[:-1], np.abs(smooth(sorted_gf_omega[:-1])[:,1]), color=col)
-            plt.plot(sorted_desired_omega, np.abs(sorted_gf_hat_sm[:,1]), '.', color=col)
-        gf_hat_sm = np.concatenate((sorted_gf_hat_sm[-ind:], sorted_gf_hat_sm[:-ind]))
-        new_gfs.append(np.fft.ifft(gf_hat_sm, axis=0) / dt)
+    for gf_h in new_gfs_hat:
+        new_gfs.append(np.fft.ifft(gf_h, axis=0) / dt)
+
     if PLOT:
         plt.legend()
         plt.show()
